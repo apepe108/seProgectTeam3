bb3a6f471d9b87b143a64df2e1966830f4677730
pmns5 <75480428+pmns5@users.noreply.github.com>
2020-12-11 16:39:56 +0100

    src updated

--- frontEnd/FrontEnd/js/EWOSelectionController.js
+++ frontEnd/FrontEnd/js/EWOSelectionController.js
@@ -1,4 +1,4 @@
-class ActivitySelectionController {
+class EWOSelectionController {
 
     /**
      * Constructor
@@ -12,7 +12,8 @@ class ActivitySelectionController {
         this.viewEndPoint = endPoint + "/activity";
         this.editEndPoint = endPoint + "/edit-activity";
         this.smpEndPoint = endPoint + "/view-smp";
-        this.materialsEndPoint = endPoint + "/material";
+        this.materialsEndPoint = endPoint + "/material"
+        this.competencesViewEndpoint = endPoint + "/competencies"
 
         this.selectedRowID = -1;
         this.doSelect = false;
@@ -28,23 +29,22 @@ class ActivitySelectionController {
      * @use renderGUI()
      */
     fillTable() {
-
         let controller = this;
+
         let week = $('#week-selection').val();
         let year = $('#year-selection').val();
+        let day = $('#day-selection').val();
         let type = this.type;
 
-        if(week === "0" || year === ""){
-            controller.renderAlert('Error. Select both year and week before continuing', false);
+        if (week === "0" || year === "" || day === "") {
+            controller.renderAlert('Error. Select all fields before continuing', false);
             return;
         }
-
-
         /* Call the microservice and evaluate data and result status */
-        $.getJSON(this.viewEndPoint, {week:week, year:year, type:this.type} ,function (data) {
+        $.getJSON(this.viewEndPoint, {day:day, week: week, year: year, type: type}, function (data) {
             controller.renderGUI(data);
         }).done(function () {
-           // controller.renderAlert('Data charged successfully.', true);
+            // controller.renderAlert('Data charged successfully.', true);
         }).fail(function () {
             controller.renderAlert('Error while charging data. Retry in a few second.', false);
         });
@@ -70,18 +70,18 @@ class ActivitySelectionController {
             row = row.replace(/{ID}/ig, obj.id);
             row = row.replace(/{Site}/ig, obj.site.name);
             row = row.replace(/{Typology}/ig, obj.typology.name);
-            row = row.replace(/{Time}/ig, obj.estimatedInterventionTime);
 
             let selectIcon = '<button class="btn btn-outline-primary btn-sm"  data-toggle="modal"\n' +
-                '                data-target="#select-modal" onclick="controller.doSelect=true">'+
+                '                data-target="#select-modal" onclick="controller.doSelect=true">' +
                 '        Select </button>';
             row = row.replace(/{Select}/ig, selectIcon);
 
             $('#table-rows').append(row);
         });
+
         /* When empty address-book */
         if (data.length === 0) {
-            $("tfoot").html('<tr><th colspan="4">No records</th></tr>');
+            $("tfoot").html('<tr><th colspan="3">No records</th></tr>');
         } else {
             $("tfoot tr:first").fadeOut(100, function () {
                 $("tfoot tr:first").remove();
@@ -112,55 +112,66 @@ class ActivitySelectionController {
     };
 
     /**
-     * Open select Modal, download Json data and render.
+     * Open Select Modal, download Json data and render.
      */
     viewSelect() {
         let controller = this
+
         controller.addMaterialsChoices();
+        controller.addCompetencesChoices();
         $.get(controller.viewEndPoint, {id: controller.selectedRowID}, function (data) {
-            let activity;
-            activity = data.id  + " - " + data.site.name + " - " + data.typology.name + " - " + data.estimatedInterventionTime +"'";
+            let activity = "";
+            activity = data.id + " - " + data.site.name + " - " + data.typology.name;
             $('#select-id').val(data.id);
             $('#select-type').val(controller.type);
             $('#select-site').val(data.site.id);
-            $('#select-procedure').val(data.maintenanceProcedures.id);
             $('#select-typology').val(data.typology.id);
             $('#select-time').val(data.estimatedInterventionTime);
-            $('#select-interruptibility').val(data.interruptibility);
+            if (data.interruptibility === "true") {
+                $('#select-interruptibility-true').prop('checked', 'true');
+            } else {
+                $('#select-interruptibility-false').prop('checked', 'true');
+            }
 
+            $('#select-day-number').val(data.day)
+            $('#select-day').val(controller.getDay(data.day));
             $('#select-week').val(data.week);
             $('#select-year').val(data.year);
-            $("#select-activity").val(activity);
+            $("#select-activity").val(activity)
 
             $('#select-description').val(data.description);
 
             if (data.workspace.id != 0) {
                 $('#select-notes').val(data.workspace.description);
             }
-
-            $('#select-skills li').remove();
-            $.each(data.skill, function (index, obj) {
-                $('#select-skills').append("<li >"+obj.name+"</li>");
-            });
-
-            let smpSelector = '#smp-icon';
-            if (data.maintenanceProcedures.smp !== "0"){
-                $(smpSelector).prop('disabled', false);
-                $(smpSelector).val(data.maintenanceProcedures.smp);
-                $('#smp-name').text(data.maintenanceProcedures.name);
-            }else{
-                $(smpSelector).prop('disabled', true);
-                $('#smp-name').text('No SMP File Attached');
-            }
-
             controller.markMaterialsAlreadyPossessed(data.materials);
-        }).done(function () {
+        }).done(function (data) {
 
         });
         // Charging
     }
 
 
+    /**
+     * Adds a checkbox for each element of the Json data
+     *
+     */
+    addCompetencesChoices() {
+        let controller = this;
+        $.getJSON(controller.competencesViewEndpoint, function (data) {
+            $('#table-competence td').remove();
+            let staticHtml = $("#competence-template").html();
+            $.each(data, function (index, obj) {
+                let elem = staticHtml;
+                elem = elem.replace(/{Competence}/ig,
+                    "<div class='container d-inline-block'>" +
+                    "<input type='checkbox' name='skill-id' value='" + obj.id + "'>\t" +
+                    "<td>" + obj.name + "</td>" +
+                    "</div>");
+                $('#competence-rows').append(elem);
+            });
+        });
+    }
 
     /**
      * Adds a checkbox for each element of the Json data
@@ -204,22 +215,15 @@ class ActivitySelectionController {
      * Send edit request. If it succeeds, redirect to the next page for the assignment.
      *
      */
-    forward(){
+    forward() {
         let controller = this;
-        $.post(this.editEndPoint, $('#select-form').serialize(), function(data){
+        $.post(this.editEndPoint, $('#select-form').serialize(), function (data) {
 
-        }).done(function(){
+        }).done(function () {
             let data = "?activity-id=" + controller.selectedRowID + "&type=" + controller.type;
-            window.location.replace('MaintainerAssignment.html' + data)
+            window.location.replace('MaintainerAssignment.html' + data);
         });
 
-    }
-
-    /**
-     * Perform a get request for the selected file
-     */
-    smp_request(){
-        window.open(this.smpEndPoint + '?id=' + $('#smp-icon').val() , '_blank');
     }
 
     /**
@@ -229,6 +233,33 @@ class ActivitySelectionController {
         for (let i = 1; i < 53; i++) {
             let row = "<option name='week' value='" + i + "'>" + i + "</option>";
             $('#week-selection').append(row);
+        }
+    }
+
+    /**
+     * Given the number of the day of the week, returns its name
+     *
+     * @param number the number of the day of the week
+     * @returns {string} the name of the day of the week
+     */
+    getDay(number) {
+        switch (number) {
+            case "1":
+                return "Monday";
+            case "2":
+                return "Tuesday";
+            case "3":
+                return "Wednesday";
+            case "4":
+                return "Thursday";
+            case "5":
+                return "Friday";
+            case "6":
+                return "Saturday";
+            case "7":
+                return "Sunday";
+            default:
+                return "";
         }
     }
 
@@ -247,8 +278,7 @@ class ActivitySelectionController {
             controller.doSelect = false;
             controller.viewSelect();
         }
-
-    };
+    }
 
     /**
      * Function used when no one row have to be selected (e.g., when a role is successfully created or edited).
@@ -256,7 +286,6 @@ class ActivitySelectionController {
     unselect() {
         $('.selected').removeClass('selected');
         controller.setSelectedRowID(-1);
-    };
-
+    }
 
 }
