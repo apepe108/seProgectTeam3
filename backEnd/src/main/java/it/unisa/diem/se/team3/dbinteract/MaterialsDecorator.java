5ae80c24e942079b672b392db559f7381f2ca2ac
apepe108 <74818967+apepe108@users.noreply.github.com>
2020-12-12 20:14:24 +0100

    Add files via upload

--- backEnd/src/main/java/it/unisa/diem/se/team3/dbinteract/MaterialsDecorator.java
+++ backEnd/src/main/java/it/unisa/diem/se/team3/dbinteract/MaterialsDecorator.java
@@ -24,6 +24,26 @@ public class MaterialsDecorator extends DbDecorator{
     }
 
     /**
+     * This method allows add a tuple to the materials table in the db. It takes the fields of the tuple as a parameter.
+     *
+     * @param name:        the name of the materials.
+     * @param description: the description of the materials.
+     * @return true if no error occur, else false.
+     */
+    public boolean addMaterials(@NotNull String name, @NotNull String description) {
+        try (PreparedStatement stmt = getConn().prepareStatement(
+                "INSERT INTO materials(id, name, description) VALUES (nextval('materials_id'), ?, ?);"
+        )){
+            stmt.setString(1, name);
+            stmt.setString(2, description);
+            stmt.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * This method displays a list of Materials elements, which represent data from the materials and related tables.
      *
      * @return a list of Materials Model or null if an error occur.
@@ -40,5 +60,67 @@ public class MaterialsDecorator extends DbDecorator{
             return null;
         }
         return result;
+    }
+
+    /**
+     * This method displays an Material elements with the selected id, which represent data from the
+     * materials tables.
+     *
+     * @param id: the material id;
+     * @return a Material object or null if an error occur.
+     */
+    public Materials getMaterial(long id) {
+        try (PreparedStatement stmt = getConn().prepareStatement("SELECT * FROM materials WHERE id = ?;")){
+            stmt.setLong(1, id);
+            try (ResultSet rs = stmt.executeQuery()){
+                if (!rs.next()) {
+                    return null;
+                }
+                return new Materials(rs.getLong("id"), rs.getString("name"),
+                        rs.getString("description"));
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    /**
+     * This method delete a tuple from the materials table based on its id.
+     *
+     * @param id: id of the tuple to delete.
+     * @return true if no error occur, else false.
+     */
+    public boolean deleteMaterials(long id) {
+        try (PreparedStatement stmt = getConn().prepareStatement(
+                "DELETE FROM materials WHERE id = ?;"
+        )){
+            stmt.setLong(1, id);
+            stmt.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method edit a tuple from the maintainer_role table based on its id. Need the parameter to edit.
+     *
+     * @param id:          the id of the tuple to edit.
+     * @param name:        the new materials name.
+     * @param description: the new materials description.
+     * @return true if no error occur, else false.
+     */
+    public boolean editMaterials(long id, @NotNull String name, @NotNull String description) {
+        try (PreparedStatement stmt = getConn().prepareStatement(
+                "UPDATE materials SET name = ?, description = ? WHERE id = ?"
+        )){
+            stmt.setString(1, name);
+            stmt.setString(2, description);
+            stmt.setLong(3, id);
+            stmt.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
     }
 }
--- backEnd/src/test/java/it/unisa/diem/se/team3/servlet/MaterialsServletTest.java
+++ backEnd/src/test/java/it/unisa/diem/se/team3/servlet/MaterialsServletTest.java
@@ -63,11 +63,142 @@ class MaterialsServletTest {
     }
 
     @Test
-    void doGet() throws IOException {
-        // Test GET
+    void doPostCreate() throws IOException {
+        // TRY POST
+        String urlParameters = "name=New Material&description=Description new material.";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+        String request = "http://localhost:8080/create-material";
+        URL url = new URL(request);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setDoOutput(true);
+        http.setRequestMethod("POST");
+        http.setRequestProperty("charset", "utf-8");
+        http.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        try (DataOutputStream wr = new DataOutputStream(http.getOutputStream())) {
+            wr.write(postData);
+        }
+        assertEquals(HttpStatus.OK_200, http.getResponseCode());
+
+        // Verify correct insert
+        String expected = "[{\"id\":\"1\",\"name\":\"Material 1\",\"description\":\"Description material 1\"},{\"id\":\"2\",\"name\":\"Material 2\",\"description\":\"Description material 2\"},{\"id\":\"3\",\"name\":\"Material 3\",\"description\":\"Description material 3\"},{\"id\":\"4\",\"name\":\"New Material\",\"description\":\"Description new material.\"}]";
+        assertEquals(expected, JsonUtil.toJson(db.getMaterials()));
+    }
+
+
+    @Test
+    void doPostCreateMissingParameters() throws IOException {
+        // TRY POST
+        String urlParameters = "name=New Material";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+        String request = "http://localhost:8080/create-material";
+        URL url = new URL(request);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setDoOutput(true);
+        http.setRequestMethod("POST");
+        http.setRequestProperty("charset", "utf-8");
+        http.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        try (DataOutputStream wr = new DataOutputStream(http.getOutputStream())) {
+            wr.write(postData);
+        }
+        assertEquals(HttpStatus.BAD_REQUEST_400, http.getResponseCode());
+    }
+
+    @Test
+    void doGetAll() throws IOException, InterruptedException {
+        // TRY GET
         HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/material").openConnection();
         http.connect();
+
         assertEquals(HttpStatus.OK_200, http.getResponseCode());
-        assertEquals("[{\"id\":\"1\",\"name\":\"Material 1\",\"description\":\"Description material 1\"},{\"id\":\"2\",\"name\":\"Material 2\",\"description\":\"Description material 2\"},{\"id\":\"3\",\"name\":\"Material 3\",\"description\":\"Description material 3\"}]", tester.readPage(http));
+
+        String expected = "[{\"id\":\"1\",\"name\":\"Material 1\",\"description\":\"Description material 1\"},{\"id\":\"2\",\"name\":\"Material 2\",\"description\":\"Description material 2\"},{\"id\":\"3\",\"name\":\"Material 3\",\"description\":\"Description material 3\"}]";
+        assertEquals(expected, tester.readPage(http));
+    }
+
+    @Test
+    void doGetOne() throws IOException {
+        // TRY GET
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/material?id=1").openConnection();
+        http.connect();
+
+        assertEquals(HttpStatus.OK_200, http.getResponseCode());
+
+        String expected = "{\"id\":\"1\",\"name\":\"Material 1\",\"description\":\"Description material 1\"}";
+        assertEquals(expected, tester.readPage(http));
+    }
+
+    @Test
+    void doGetOneNoExisting() throws IOException {
+        // TRY GET
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/material?id=6").openConnection();
+        http.connect();
+
+        assertEquals(HttpStatus.EXPECTATION_FAILED_417, http.getResponseCode());
+    }
+
+    @Test
+    void doGetDelete() throws IOException {
+        // TRY GET
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/delete-material?id=1").openConnection();
+        http.connect();
+
+        assertEquals(HttpStatus.OK_200, http.getResponseCode());
+
+        // Verify correct delete
+        String expected = "[{\"id\":\"2\",\"name\":\"Material 2\",\"description\":\"Description material 2\"},{\"id\":\"3\",\"name\":\"Material 3\",\"description\":\"Description material 3\"}]";
+        assertEquals(expected, JsonUtil.toJson(db.getMaterials()));
+    }
+
+    @Test
+    void doGetDeleteNoId() throws IOException {
+        // TRY GET
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/delete-material").openConnection();
+        http.connect();
+
+        assertEquals(HttpStatus.BAD_REQUEST_400, http.getResponseCode());
+    }
+
+    @Test
+    void doPostEdit() throws IOException {
+        // TRY POST
+        String urlParameters = "id=1&name=New Material&description=New description of material";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+        String request = "http://localhost:8080/edit-material";
+        URL url = new URL(request);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setDoOutput(true);
+        http.setRequestMethod("POST");
+        http.setRequestProperty("charset", "utf-8");
+        http.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        try (DataOutputStream wr = new DataOutputStream(http.getOutputStream())) {
+            wr.write(postData);
+        }
+        assertEquals(HttpStatus.OK_200, http.getResponseCode());
+
+        // Verify correct insert
+        String expected = "[{\"id\":\"1\",\"name\":\"New Material\",\"description\":\"New description of material\"},{\"id\":\"2\",\"name\":\"Material 2\",\"description\":\"Description material 2\"},{\"id\":\"3\",\"name\":\"Material 3\",\"description\":\"Description material 3\"}]";
+        assertEquals(expected, JsonUtil.toJson(db.getMaterials()));
+    }
+
+    @Test
+    void doPostEditMissingParameters() throws IOException {
+        // TRY POST
+        String urlParameters = "name=New Material&description=New description of material";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+        String request = "http://localhost:8080/edit-material";
+        URL url = new URL(request);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setDoOutput(true);
+        http.setRequestMethod("POST");
+        http.setRequestProperty("charset", "utf-8");
+        http.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+        try (DataOutputStream wr = new DataOutputStream(http.getOutputStream())) {
+            wr.write(postData);
+        }
+        assertEquals(HttpStatus.BAD_REQUEST_400, http.getResponseCode());
     }
 }
