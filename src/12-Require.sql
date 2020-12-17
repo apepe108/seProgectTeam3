1cb6ae97873c91010bb19dccf8a2809b32f43673
ter98-tort <75849268+ter98-tort@users.noreply.github.com>
2020-12-17 11:37:58 +0100

    Delete 12-Require.sql

--- src/12-Require.sql
+++
@@ -1,8 +0,0 @@
---Table: require
-
---DROP TABLE IF EXISTS require CASCADE;
-CREATE TABLE require(
-	competences     			 BIGINT REFERENCES competences(id),
-	maintenance_procedures  	 BIGINT REFERENCES maintenance_procedures(id),
-	PRIMARY KEY (competences,maintenance_procedures)
-);
