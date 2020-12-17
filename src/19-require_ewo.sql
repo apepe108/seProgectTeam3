89f981ca9a073dd984245959379dfd9045303fe4
pmns5 <75480428+pmns5@users.noreply.github.com>
2020-12-17 11:36:26 +0100

    Delete 19-require_ewo.sql

--- src/19-require_ewo.sql
+++
@@ -1,8 +0,0 @@
--- Table: Require ewo
-
---DROP TABLE IF EXISTS require_ewo;
-CREATE TABLE require_ewo (
-	activity 			BIGINT NOT NULL REFERENCES activity(id),
-	competences			BIGINT NOT NULL REFERENCES competences(id),
-	PRIMARY KEY (activity, competences)
-);
