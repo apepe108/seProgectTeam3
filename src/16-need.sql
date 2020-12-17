043128ba12a215e329292f3f9b07f3b31ee79757
ter98-tort <75849268+ter98-tort@users.noreply.github.com>
2020-12-17 11:37:03 +0100

    Delete 16-need.sql

--- src/16-need.sql
+++
@@ -1,8 +0,0 @@
---Table: need
-
---DROP TABLE IF EXISTS need CASCADE
-CREATE TABLE need (
-	activity		BIGINT REFERENCES activity(id),
-	materials		BIGINT REFERENCES materials(id),
-	PRIMARY KEY (activity, materials)
-)
